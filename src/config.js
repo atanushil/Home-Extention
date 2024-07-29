73045832cf0bc0a3a4695129c6d4f16dc656ae1f
Atanu Shil <129635236+atanushil@users.noreply.github.com>
2024-07-29 08:10:09 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  WEATHER_API_KEY: "399e3ef61cmshfb9b26c31bd8b00p105d3ajsn639dc3a8023c",
-  PROGRAMMABLE_SEARCH_ENGINE_ID: "975d312e6d6504f03",
+  WEATHER_API_KEY: "399e3ef61cmshfb9b26c31bd8b00p105d3ajsn639dc3a8023c", // add your own api key 
+  PROGRAMMABLE_SEARCH_ENGINE_ID: "975d312e6d6504f03", // add your own search engine id
 };
 export default config;
